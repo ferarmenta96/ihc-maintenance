6e53876df52c262f652e6e228b543df9e7ada1fc
ferarmenta96 <ferarmentaus96@gmail.com>
2026-03-24 21:21:16 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -14,7 +14,7 @@
 
 const IHC_CONFIG = {
   // 🔴 REPLACE THIS URL with your deployed Apps Script URL
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxPBOEzIaaxk0fJwSAMz20SV8p9EG8httWAjWsFqbc5-049nUenzZj_lb092VVI-BLV/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzLCFELqXeaZLbEbx1--MfKsUM7ipNumL9nlmhZkyvHBiQ085qFZW7Qnc6nNj9v3zEC/exec",
 
   APP_NAME:   "IHC Maintenance",
   VERSION:    "1.0.0",
